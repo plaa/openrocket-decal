708611b468a9bd9c1d145033edb150eee87e7ccf
Kevin Ruland <kruland@sunflower.com>
2012-07-20 04:20:11 +0000

    Fixed loading decals from example file.  Added FileInfo class which is used to describe data about the file - what the URL is to it and it's parent directory (if appropriate).  The FileInfo class is pushed into the DecalRegistry to facilitate loading decals from the correct location.  Added the Apocalypse with decals as an example file.

--- core/src/net/sf/openrocket/file/GeneralRocketLoader.java
+++ core/src/net/sf/openrocket/file/GeneralRocketLoader.java
@@ -49,7 +49,7 @@ public class GeneralRocketLoader {
 		try {
 			
 			stream = new BufferedInputStream(new FileInputStream(source));
-			OpenRocketDocument doc = load(stream, source, motorFinder);
+			OpenRocketDocument doc = load(stream, new FileInfo(source), motorFinder);
 			return doc;
 			
 		} catch (Exception e) {
@@ -65,10 +65,10 @@ public class GeneralRocketLoader {
 		}
 	}
 	
-	public final OpenRocketDocument load(InputStream source, File file, MotorFinder motorFinder) throws RocketLoadException {
+	public final OpenRocketDocument load(InputStream source, FileInfo fileInfo, MotorFinder motorFinder) throws RocketLoadException {
 		try {
 		OpenRocketDocument doc = loadFromStream(source, motorFinder );
-		doc.getDecalRegistry().setBaseFile(file);
+		doc.getDecalRegistry().setBaseFile(fileInfo);
 		return doc;
 		} catch (Exception e) {
 			throw new RocketLoadException("Exception loading stream", e);
